4c683c29729db68b4aaa2e36c23cdc1f388aa0f6
John Horton <john.joseph.horton@gmail.com>
2013-07-23 07:48:48 -0700

    Connect to DB

---
+++ R/connectDB.R
@@ -0,0 +1,14 @@
+#' Creates a connection to the DB and returns connection as a global variable, con
+#'
+#' @param credentials - list containing connection information 
+#' @return None - run for side-effects.  
+#' @export
+
+connectDB <- function(credentials){
+    con <<-  with(credentials, dbConnect(dbDriver(dbdriver),
+                                    dbname=dbname,
+                                    host = host,
+                                    user = user,
+                                    password = password,
+                                    port = port))
+}
