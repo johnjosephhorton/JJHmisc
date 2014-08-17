0d985aaff8fef65b4d90b126b52f93434ccb5227
John Horton <john.joseph.horton@gmail.com>
2014-08-17 12:47:08 -0400

    Tool for adding parameters to a latex writeup

---
+++ R/genParamAdder.R
@@ -0,0 +1,19 @@
+#' A function for generating a function that adds parameters to a tex file (parameter.file)
+#' which is imported in the base LaTeX document. It is useful for passing numbers, dates and so on
+#' programmatically to a write-up rather than writing them in by hand. 
+#'
+#' @param parameter.file file you want to hold the customer latex new commands
+#' @return A function that can be used to add parameters to the file created. 
+#' @export
+
+genParamAdder <- function(parameter.file){
+    if (file.exists(parameter.file)) {
+        file.remove(parameter.file)
+    }
+    f <- function(name, value){
+        line <- paste0("\\newcommand{", name, "}{", value, "}")
+        write(line, parameter.file, ncolumns = 1, append = TRUE)
+    }
+    f
+}
+
